16672afba78949c190fec38b1fbe79387d148646
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-05-11 08:59:42 +0200

    adjusted styles for isTyping

--- client/src/Components/IsTyping.tsx
+++ client/src/Components/IsTyping.tsx
@@ -4,14 +4,14 @@ import { useSockets } from "../Context/Socket.context";
  const IsTyping = () => {
     const { isTyping, username } = useSockets();
 
-    return <div style={rootStyle}>{isTyping}...</div>
+    return <div style={rootStyle}>{username} is typing...</div>
 };
 
 const rootStyle: CSSProperties = {
     color: '#444',
     marginLeft: '1.3rem',
-    fontSize: '1.1rem',
-    fontWeight: 'bold',
+    fontSize: '.8rem',
+    fontWeight: 'thin',
     
 }
 export default IsTyping;
