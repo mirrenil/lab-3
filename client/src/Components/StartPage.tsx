65cf011395d78a6208f9fabc785442fe2d66bfce
linnea <linnea.albertsson@gmail.com>
2022-05-12 10:23:58 +0200

    Fixed so login covers whole page

--- client/src/Components/StartPage.tsx
+++ client/src/Components/StartPage.tsx
@@ -25,7 +25,9 @@ const rootstyle: CSSProperties = {
 };
 const usernameInputDivStyle: CSSProperties = {
   position: "absolute",
+  zIndex: "100",
   top: 0,
+  left: 0,
   height: "100vh",
   width: "100%",
   background: "rgba(0, 0, 0, 0.9)",
